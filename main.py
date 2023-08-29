f0e6f5d0f68ca2f4d261b1ff8c51480076580346
Sam Padilla <padilla.samuelk@gmail.com>
2023-08-29 19:06:47 +0000

    Adding optimistic secret generation to minimize needed service account permissions

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import json
 import os
 import re
 import hashlib
+import sys
 
 from dotenv import load_dotenv #type: ignore
 from google.cloud import secretmanager
@@ -32,7 +33,7 @@ def create_secrets(secrets_configs:list):
     """
 
     #Unpack configs
-    project_id, key_directory_path, _, output_dir = secrets_configs
+    project_id, key_directory_path, _, output_dir, optimistic = secrets_configs
 
     #Create client and storing dict
     client = secretmanager.SecretManagerServiceClient()
@@ -55,28 +56,21 @@ def create_secrets(secrets_configs:list):
                 f.close()
             payload_bytes = contents.encode("UTF-8")
 
-            #Calculate string SHA256.
-            str_sha256 = hashlib.sha256(contents.encode())
-
             #Add secret version
             request={"parent": f"projects/{project_id}/secrets/{key_file_name}",
-                     "payload": {"data": payload_bytes}}
+                    "payload": {"data": payload_bytes}}
             version = client.add_secret_version(request=request)
 
-            #Access the secret version and verify payload SHA256
-            response = client.access_secret_version(request={"name": version.name})
+            # Add secret and save keys and name to file if optimistic.
+            if optimistic:
+                pubkeys_names[json.loads(contents)["pubkey"]] = key_file_name
 
-            #Get payload sha256
-            payload = response.payload.data.decode("UTF-8")
-            payload_sha256 = hashlib.sha256(payload.encode())
-
-            #If checksum verifies, store pubkey and secret locally
-            if str_sha256.digest() == payload_sha256.digest():
-                print("\t[✓] Matching checksums of secret manager and local data.")
-                pubkeys_names[json.loads(payload)["pubkey"]] = key_file_name
+            #Else calculate string SHA256
             else:
-                print("\t[x] Data corruption detected. Panicing.")
-                exit(1)
+                pubkeys_names = verify_payload(client, version,
+                                               pubkeys_names,
+                                               key_file_name,
+                                               contents)
 
             i += 1
 
@@ -88,18 +82,35 @@ def create_secrets(secrets_configs:list):
     print(f"\t[✓] Done. Check {output_dir}")
 
 # Helper Functions #
-def validate_env_and_params():
+def validate_env_and_params(params):
     """
     Validates the execution environment and parameters:
-        1. Checks validity of project_id
-        2. Checks that key directory exists
-        2. Checks that output directory exists
-        3. Checks that and prompts for overwrite
+        1. Checks optimistic optional parameter
+        2. Checks validity of project_id
+        3. Checks that key directory exists
+        4. Checks that output directory exists
+        5. Checks that and prompts for overwrite
             confirmation if local stores exist already
-        4. Checks application default credentials file exists and its format
+        6. Checks application default credentials file exists and its format
     Returns True if all are valid, false otherwise.
     """
     #Parameters
+    optimistic = False
+    if len(params) > 2:
+        print("ERROR: Two many parameters.",
+              "\nOnly accepted parameter is \'optimistic\'.",
+                "\nSet this flag if you don't want checksum verification of uploaded data.")
+        return []
+    if len(params) == 2:
+        if params[1] != "optimistic":
+            print("ERROR: Invalid optional parameter.",
+                  "\nOnly accepted parameter is \'optimistic\'.",
+                    "\nSet this flag if you don't want checksum verification of uploaded data.")
+            return []
+        else:
+            optimistic = True
+
+    #Configs
     project_id, key_directory_path, google_adc, output_dir = get_configs()
 
     #Project ID
@@ -124,7 +135,8 @@ def validate_env_and_params():
     #Confirm overwrite
     exists_pf = os.path.exists(f"{output_dir}public_keys.txt")
     exists_nf = os.path.exists(f"{output_dir}secret_names.txt")
-    if exists_nf or exists_pf:
+    exists_ptnf = os.path.exists(f"{output_dir}pubkey_to_names.txt")
+    if exists_nf or exists_pf or exists_ptnf:
         input_message = f"[WARN] There are public keys and secret files already in {output_dir}.\n\tDo you want to overwrite it? (yes only - anything else will halt.)\n\t\t"
         response = input(input_message)
         if response.lower() != 'yes':
@@ -152,7 +164,7 @@ def validate_env_and_params():
                 f"\nPlease ensure the file has all the required keys: {required_keys}.")
         return []
 
-    return [project_id, key_directory_path, google_adc, output_dir]
+    return [project_id, key_directory_path, google_adc, output_dir, optimistic]
 
 def get_configs():
     """Opens the .env file and loads configs"""
@@ -192,16 +204,59 @@ def save_validator_pubkey_and_name(pubkeys_to_names:dict, output:str):
         output: Chosen output directory in the config.
     """
     #Write files
-    with open(f"{output}public_keys.txt", "w", encoding="utf-8") as pf, open(f"{output}secret_names.txt", "w", encoding="utf-8") as nf:
+    with open(f"{output}public_keys.txt", "w", encoding="utf-8") as pf, open(f"{output}secret_names.txt", "w", encoding="utf-8") as nf, open(f"{output}pubkey_to_names.txt", "w", encoding="utf-8") as ptnf:
         for pubkey, name in pubkeys_to_names.items():
             pf.write(f"{pubkey}\n")
             nf.write(f"{name}\n")
+            ptnf.write(f"{pubkey} : {name}\n")
         pf.close()
         nf.close()
+        ptnf.close()
+
+def verify_payload(client: secretmanager.SecretManagerServiceClient,
+                   version: secretmanager.SecretVersion,
+                   pubkeys_names: dict,
+                   secret_name: str,
+                   contents: str):
+    """
+    Verifies the sha256 checkcsum of the contents of keystore file, compared
+    to the hash of the created secret. If succesful, it stores the pubkey and
+    secret name locally for records. It panics otherwise.
+
+    Args:
+        client: the Secret manager client
+        version: the Secret version getting checked against
+        pubkey_names: the dictionary where local data is getting stored
+        secret_name: the name of the secret
+        contents: the contents of the validator keystore to be checked against
+            the secret.
+    
+    Returns: An updated dictionary with the new pubkey->secret name mapping
+    """
+
+    #Get string sha256
+    str_sha256 = hashlib.sha256(contents.encode())
+    
+    #Access the secret version and verify payload SHA256
+    response = client.access_secret_version(request={"name": version.name})
+
+    #Get payload sha256
+    payload = response.payload.data.decode("UTF-8")
+    payload_sha256 = hashlib.sha256(payload.encode())
+
+    #If checksum verifies, store pubkey and secret locally
+    if str_sha256.digest() == payload_sha256.digest():
+        print("\t[✓] Matching checksums of secret manager and local data.")
+        pubkeys_names[json.loads(payload)["pubkey"]] = secret_name
+    else:
+        print("\t[x] Data corruption detected. Panicing.")
+        exit(1)
+    
+    return pubkeys_names
 
 # Main Caller #
 if __name__ == "__main__":
-    configs = validate_env_and_params()
+    configs = validate_env_and_params(sys.argv)
     if not configs:
         exit(3)
     create_secrets(configs)
