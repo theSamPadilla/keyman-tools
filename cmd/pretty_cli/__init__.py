9c06a59f2b94dcfe72d6fef0d4c24193008d4430
Sam Padilla <padilla.samuelk@gmail.com>
2023-09-15 21:07:49 +0000

    Refactoring colors outside of __init__

--- cmd/pretty_cli/__init__.py
+++ cmd/pretty_cli/__init__.py
@@ -1,20 +1,5 @@
 import json
-
-# Color Variables
-end = "\033[0m"
-bold = "\033[1m"
-gray = "\033[30m"
-red = "\033[31m"
-green = "\033[32m"
-yellow = "\033[33m"
-blue = "\033[34m"
-pink = "\033[35m"
-bg_black = "\033[40m"
-bg_red = "\033[41m"
-bg_green = "\033[42m"
-bg_yellow = "\033[43m"
-bg_blue = "\033[44m"
-bg_pink = "\033[45m"
+from cmd.pretty_cli.colors import *
 
 # Get and verify config.json
 def get_cmd_config():
@@ -57,7 +42,6 @@ p_commands = cmd_config["usage"]["commands"] #? bool
 p_command_flags = cmd_config["usage"]["command-flags"] #? bool
 p_subcommands = cmd_config["usage"]["subcommands"] #? bool
 p_subcommand_flags = cmd_config["usage"]["subcommand-flags"] #? bool
-p_output_files = cmd_config["usage"]["output-files"] #? bool
 
 # Usage string
 usage_string = get_usage_string() #? str
@@ -65,4 +49,3 @@ usage_string = get_usage_string() #? str
 # Commands, global_flags, and output dicts
 global_flags = cmd_config["global-flags"] #? dict
 commands = cmd_config["commands"] #? dict
-output_files = cmd_config["output-files"] #? dict
