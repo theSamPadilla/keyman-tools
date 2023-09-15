883848a0a50345232c90fa3f82c1486ed2bcbacc
Sam Padilla <samuelpadilla@google.com>
2023-09-15 04:52:07 +0000

    Adding web3signer package

---
+++ web3signer/handler.py
@@ -0,0 +1,6 @@
+""" Receives calls from main, handles validation, and routes to appropriate subcommand"""
+
+def handler(command_flags, subcommand):
+    print("You are in the web3signer handler...")
+    print("This is a construction zone, come back soon")
+    return
