b9f514d3a7cd0bec73236cef8a679f3ffe8054e2
Sam Padilla <padilla.samuelk@gmail.com>
2023-09-15 21:07:01 +0000

    Refactoring colors on pretty_cli

---
+++ cmd/pretty_cli/colors.py
@@ -0,0 +1,17 @@
+"""cli colors for pretty_cli"""
+
+# Color Variables
+end = "\033[0m"
+bold = "\033[1m"
+gray = "\033[30m"
+red = "\033[31m"
+green = "\033[32m"
+yellow = "\033[33m"
+blue = "\033[34m"
+pink = "\033[35m"
+bg_black = "\033[40m"
+bg_red = "\033[41m"
+bg_green = "\033[42m"
+bg_yellow = "\033[43m"
+bg_blue = "\033[44m"
+bg_pink = "\033[45m"
