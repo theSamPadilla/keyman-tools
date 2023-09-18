d373c7169cdd668d125120c4e19631977f517bc6
Sam Padilla <padilla.samuelk@gmail.com>
2023-09-18 03:17:30 +0000

    Implementing web3signer setup-db

--- web3signer/setup_db/handler.py
+++ web3signer/setup_db/handler.py
@@ -6,11 +6,10 @@ import sys
 import web3signer.utilities as util
 import web3signer.setup_db.validation_logic as sdb_logic
 
-from cli import tool_name
-from cli.pretty.colors import bold, end, green, red, pink
+from cli.utilities import print_usage_string
+from cli.pretty import red, end
 
-
-def handler(subcommand_flags: list):
+def handler(subcommand_flags: list, authorize: bool):
     """
     Handles slashing-protection db setup.
         1. Checks if docker and web3signer are installed
@@ -18,30 +17,44 @@ def handler(subcommand_flags: list):
         3. Checks if slashing-protection is running, spins up if not.
         4. Applies the migrations on the first time the db is spun up.
     """
-    # Check if docker is installed
-    print("[INFO] Checking dependencies.")
-    dependencies = ["docker", "web3signer"]
-    for c in dependencies:
-        if not util.check_command_is_installed(c):
-            print(f"\n[ERROR] Missing required dependency {bold}{c}{end}.",
-                f"\n\tRun {bold}{green}python3 {tool_name}{end} {red}web3signer{end} {pink}install{end}")
-            sys.exit(1)
+    # Check dependencies
+    util.check_dependencies(["docker", "web3signer"])
 
     # Check if web3signer exists
-    print("\n[INFO] Checking migrations directory")
     migrations_path = sdb_logic.check_web3signer_migrations_dir()
     if not migrations_path:
-        print("[ERROR] web3signer not found in $PATH")
+        print(f"\n{red}[ERROR]{end} web3signer not found in $PATH.",
+                f"\n\t Install it manually or run {print_usage_string('web3signer', 'install')}")
         sys.exit(1)
-    print(f"\t[✓] Found migrations at {migrations_path}")
 
     # Get db setup scripts path
     script_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "scripts")
 
-    #TODO: You are here
-    # Check if slashing-protection db is running, if running exit
-    #? If it is running, the migrations should have been applied.
+    # Check if slashing-protection db is running
+    container_name = "slashing-protection-db" # Do not change this. It is hardcoded in the bash scripts
+    container_id = sdb_logic.get_container_id(container_name)
 
-    # Get db user and pwd and create db if not
+    #? If it is running, the migrations should have been applied, return
+    if container_id:
+        print("\n\n[INFO] Slashing protection database setup complete.")
+        return
 
-    # Apply migrations
+    # Get script path
+    start_db_path = os.path.join(script_dir, "run-db.sh")
+
+    # Get db user and pwd and create db
+    user, passwd = sdb_logic.get_db_user_and_password(subcommand_flags)
+    container_id = sdb_logic.start_slashing_protection_db(container_name, migrations_path,
+                                                          user, passwd, start_db_path, authorize)
+
+    # Exit if container_id was not found, this should be an error.
+    if not container_id:
+        print(f"\n{red}[ERROR]{end} Creation or lookup of {container_name} failed.")
+        sys.exit(1)
+
+    # Get migrations script path, authorizaition, and apply migrations
+    apply_migrations_script_path = os.path.join(script_dir, "database_migration.sh")
+    sdb_logic.apply_db_migrations(container_id, migrations_path, user,
+                                  apply_migrations_script_path, authorize)
+
+    print("\n\n[INFO] Slashing protection database setup complete.")
--- web3signer/setup_db/validation_logic.py
+++ web3signer/setup_db/validation_logic.py
@@ -1,40 +1,148 @@
 """ Validation functions for web3signer setup_db """
 
 import os
+import sys
 import subprocess
-    
+
+import web3signer.utilities as util
+from cli.pretty.colors import red, end, green, bold
+
 # Check
 def check_web3signer_migrations_dir() -> str:
     """
-    Checks that the PATH location of web3signer contains the migrations directory.
+    Checks that the PATH location of web3signer contains the migrations/postgresql directory.
     Returns the directory if it exists, "" otherwise
     """
-    r = subprocess.run(["which", "web3signer"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("\n[INFO] Checking postgres migrations directory.")
+
+    # Run process and capture stdout
+    r = subprocess.run(["which", "web3signer"], stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                       text=True, check=False)
 
     # Command not found
     if r.returncode != 0:
         return ""
-    
+
     # Get the web3signer directory from $PATH location
     #? 'which web3signer' will point to the bin in the location directory
     w3s_dir = os.path.dirname(os.path.dirname(r.stdout)) #Get grandparent dir
-    
+
     # Set the migrations directory path
-    migrations_dir = os.path.join(w3s_dir, "migrations")
+    migrations_dir = os.path.join(w3s_dir, "migrations", "postgresql")
 
     # Check the location exists
     if os.path.exists(migrations_dir) and os.path.isdir(migrations_dir):
+        print(f"\t[{green}✓{end}] Found postgres migrations at {green}{migrations_dir}{end}")
         return migrations_dir
+    
+    # Return empty if the location on $PATH was not found
+    return ""
 
 # Helpers
 def get_db_user_and_password(params: list) -> tuple:
-    """Parses the params to find the user and password for the db"""
+    """
+    Parses the subcommand flags passed in params to find the user and password for the db
+    Returns: user, passwd
+    """
     user = ""
     passwd = ""
     while params:
         param = params.pop()
         if "--db-user" in param:
             user = param.split("=")[-1]
-        elif "--db-passwd" in param:
+        elif "--db-password" in param:
             passwd = param.split("=")[-1]
     return user, passwd
+
+def get_container_id(container_name: str) -> str:
+    """
+    Runs docker subprocesses to check for the container id.
+    Args:
+        container_name: The name of the container. Hardcoded on caller to be slashing-protection-db
+    Returns: The id of the container or an empty string if no container is found.
+    """
+    print(f"\n[INFO] Checking for {container_name} container ID.")
+
+    # Run process and capture stdout
+    r = subprocess.run(["docker", "ps", "--quiet", "--filter", f"name={container_name}"],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
+
+    # Command found
+    #? This throws an error because the command itself failed for some unknown reason.
+    if r.returncode != 0:
+        print(f"\n{red}[ERROR]{end} Failed to look for the slashing-protection-db")
+        sys.exit(1)
+
+    # Return id if found
+    container_id = r.stdout.strip()
+    if r.stdout:
+        print(f"\t[{green}✓{end}] Found container with id {bold}{container_id}{end}.")
+        return container_id
+    
+    print(f"\t[{red}x{end}] Container {container_name} not found.")
+    return ""
+
+# Make
+def start_slashing_protection_db(container_name: str, migrations_path: str,
+                                 usr: str, passwd: str, script_path: str, authorize: bool) -> str:
+    """
+    Starts a docker container with the slashing protection database. 
+    Args:
+        container_name: The name of the container. Hardcoded on caller to be slashing-protection-db 
+        migrations_path: The directory path to the postgresql migrations directory.
+        user: The postgres user, defined on by the subcommand flag.
+        passwd: The postgres user password, defined on by the subcommand flag.
+        script_path: Path to the docker startup script. 
+        authorize: Authorization flag. Prompts for confirmation if set to True.
+    Returns: The container ID of the created container.
+    """
+
+    print(f"\n[INFO] Starting up {container_name} container.")
+    if not authorize:
+        print("\nGrant permissions for installation below:")
+        if not util.confirm_script_execution(script_path):
+            sys.exit(0)
+
+    # Run process and output stderr and stdout
+    r = subprocess.run(["bash", script_path, migrations_path, usr, passwd],
+                         text=True, check=False)
+
+    # Running the process failed
+    if r.returncode != 0:
+        print(f"\n{red}[ERROR]{end} Failed the database startup script.")
+        sys.exit(1)
+
+    print(f"\n\n[INFO] Succesfully started {container_name}.")
+
+    # Else get and return the container id
+    return get_container_id(container_name)
+
+def apply_db_migrations(container_id: str, migrations_path: str,
+                                usr: str, script_path: str, authorize: bool):
+    """
+    Starts a docker container with the slashing protection database. 
+    Args:
+        container_name: The id of the container. 
+        migrations_path: The directory path to the postgresql migrations directory.
+        user: The postgres user, defined on by the subcommand flag.
+        script_path: Path to the docker startup script. 
+        authorize: Authorization flag. Prompts for confirmation if set to True.
+    Returns: The container ID of the created container.
+    """
+
+    print(f"\n[INFO] Applying migrations to container id {container_id}")
+    if not authorize:
+        print("\nGrant permissions for installation below:")
+        if not util.confirm_script_execution(script_path):
+            sys.exit(0)
+
+    # Run process and output stdout and stderr
+    r = subprocess.run(["bash", script_path, migrations_path, container_id, usr],
+                        text=True, check=False)
+
+    # Running the process failed
+    if r.returncode != 0:
+        print(f"\n{red}[ERROR]{end} Failed to run the database startup script.")
+        sys.exit(1)
+
+    print(f"\n[INFO] Succesfully applied migrations to {container_id}.")
