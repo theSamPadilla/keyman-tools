17db7380ce1e38d7dff036b3414c59761642e830
Sam Padilla <padilla.samuelk@gmail.com>
2023-09-25 23:33:25 +0000

    Adding sha256 generator to verify keys

---
+++ generate-key-hash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 /path/to/your/key/directory"
+    exit 1
+fi
+
+source_directory="$1"
+
+if [ ! -d "$source_directory" ]; then
+    echo "Error: Directory does not exist."
+    exit 1
+fi
+
+echo "Calculating SHA-256 hashes for files in '$source_directory'..."
+find "$source_directory" -type f -exec sha256sum {} \; | sha256sum > source_key_directory_hash.txt
+echo "SHA-256 hashes aggregated and saved to source_key_directory_hash.txt."
