faa3aec19d37b990f9b75eb3d8214af65218ffa6
Sam Padilla <padilla.samuelk@gmail.com>
2023-09-28 08:33:33 +0000

    updating sample run script for web3 signer

--- web3signer/run/run-sample-bash.sh
+++ web3signer/run/run-sample-bash.sh
@@ -9,6 +9,6 @@ nohup web3signer --key-config-path=<key-config-path-generated-by-this-tool> \
 	--http-host-allowlist=* \
 	eth2 \
 	--network=<network> \
-	--slashing-protection-db-url=<slashing-protection> \
+	--slashing-protection-db-url="jdbc:postgresql://localhost/web3signer" \
 	--slashing-protection-db-username=<username> \
-	--slashing-protection-db-password=<password> >> <path-to-log.log> 2>&1 &
+	--slashing-protection-db-password=<password> >> <path-to-log.log 2>&1 &
