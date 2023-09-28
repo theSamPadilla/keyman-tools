83ea5b6331bd44fd013c5b6ca824abf5774c220d
Sam Padilla <padilla.samuelk@gmail.com>
2023-09-28 08:45:31 +0000

    updating sample run script for web3 signer

--- web3signer/run/run-sample-bash.sh
+++ web3signer/run/run-sample-bash.sh
@@ -4,7 +4,7 @@ echo "Running web3signer..."
 # Web3 Signer
 nohup web3signer --key-config-path=<key-config-path-generated-by-this-tool> \
 	--data-path=<web3-signer-path> \
-	--http-listen-port=5432 \
+	--http-listen-port=6969 \
 	--metrics-enabled \
 	--http-host-allowlist=* \
 	eth2 \
